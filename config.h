cf78dd3ac8a628721de89fa975663e5018b1e3f7
findNextStep <findNextStep@gmail.com>
2019-04-06 14:38:12 +0800

    change font size shortcuts setting

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DroidSansMono Nerd Font:size=15";
+static char *font = "DroidSansMono Nerd Font:size=12";
 static int borderpx = 2;
 
 /*
@@ -403,8 +403,8 @@ static Shortcut shortcuts[] = {
     { ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
     { ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
     { XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-    { TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-    { TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+    { ControlMask|ShiftMask,XK_J,           zoom,           {.f = +1} },
+    { ControlMask|ShiftMask,XK_K,           zoom,           {.f = -1} },
     { TERMMOD,              XK_j,           kscrolldown,    {.i =  1} },
     { TERMMOD,              XK_k,           kscrollup,      {.i =  1} },
     { TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },

6d33010934b5e47975c89b2d6c8e376c7c6eb3a7
findNextStep <findNextStep@gmail.com>
2019-04-16 10:59:36 +0800

    change font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DroidSansMono Nerd Font:size=15";
+static char *font = "UbuntuMono Nerd Font:size=15";
 static int borderpx = 2;
 
 /*
